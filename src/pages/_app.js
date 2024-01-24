d48586313518ade0dce97384ec02aedbcd49bba0
Leandro Castro <leandro_castro980@hotmail.com>
2024-01-24 16:54:35 -0300

    finalizando

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import { ChakraProvider, CSSReset, extendTheme } from "@chakra-ui/react";
+import Head from 'next/head';
 
 const theme = extendTheme({
   fonts: {
@@ -12,6 +13,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <CSSReset />
+      <Head>
+        <title>Dom Bosco - Vila Matilde</title>
+        <link rel="icon" href="/assets/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
